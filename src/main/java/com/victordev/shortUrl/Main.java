54acd2294f0c1b83b20ae044df5545d4815b4268
João VIctor <joawrt8520@gmail.com>
2024-11-21 23:09:03 -0300

    feat: created main function handleRequest, receiving body and return UUID code

---
+++ src/main/java/com/victordev/shortUrl/Main.java
@@ -0,0 +1,38 @@
+package com.victordev.shortUrl;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+
+        String body = input.get("body").toString();
+
+        Map<String, String> bodyMap;
+
+        try {
+            bodyMap = objectMapper.readValue(body, Map.class);
+        } catch(Exception exception) {
+            throw new RuntimeException("Error parsing JSON body: " + exception.getMessage(), exception);
+        }
+
+        String originalUrl = bodyMap.get("originalUrl");
+        String expirationTime = bodyMap.get("expirationTime");
+
+        String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("codeUrl", shortUrlCode);
+
+        return response;
+    }
+}
